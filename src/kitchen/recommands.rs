b9009fd0400ef7e9db29767d58c4137a9c1325c7
SigureMo <sigure.qaq@gmail.com>
2024-02-17 05:29:03 +0800

    add reconstruct partial data logic

--- src/kitchen/recommands.rs
+++ src/kitchen/recommands.rs
@@ -1,5 +1,3 @@
-use super::metadata::EmojiItem;
-
 /// Get a list of recommended combinations of 🐾
 pub fn get_recommand_emoji_combinate_with_paw_prints() -> Vec<String> {
     vec![
@@ -32,13 +30,4 @@ pub fn get_recommand_emoji_combinate_with_paw_prints() -> Vec<String> {
     .into_iter()
     .map(|s| s.to_string())
     .collect()
-}
-
-pub fn get_paw_prints_combinations() -> EmojiItem {
-    // Run just pre-build to fetch emoji kitchen metadata from GitHub:
-    // https://raw.githubusercontent.com/xsalazar/emoji-kitchen-backend/main/app/metadata.json
-    // And prepare via jq to get the paw prints combinations
-    let raw_json_data = include_str!("paw_prints_kitchen_data.json");
-    let kitchen_data: EmojiItem = serde_json::from_str(raw_json_data).unwrap();
-    kitchen_data
 }
